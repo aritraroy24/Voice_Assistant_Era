7671efacc4c715024cff4f35897caf6d265f4229
Aritra Roy <aritraroy24@gmail.com>
2021-05-28 08:27:29 +0530

    Voice_Assistant_Era (desktop voice assistant) python project moved from python_projects repo and new repo created, README.md file modified

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# Voice_Assistant_Era
-A simple desktop voice assistant made using python if-else statement
+![](https://img.shields.io/badge/git-fff7f8?colorA=faf0f0&colorB=db4823&style=for-the-badge&logo=git)
+![](https://img.shields.io/badge/github-fff7f8?colorA=080808&colorB=8a8a8a&style=for-the-badge&logo=github)
+![](https://img.shields.io/badge/for-you-099450?colorA=b0c92e&colorB=487d3e&style=for-the-badge)
+![](https://img.shields.io/badge/python-used-bee5ed?colorA=37b6bd&colorB=3c9bb5&style=for-the-badge&logo=python)
+![](https://img.shields.io/badge/visual_studio_code-1.48.0-181717?colorA=ae36d6&style=for-the-badge&logo=visual-studio-code)
+---
+### Simple voice assistant program made with simple if-else statements :speaking_head:
+---
+# :small_orange_diamond: credentials.json :notebook_with_decorative_cover:
+### For getting access from Google Contacts using Gmail API :busts_in_silhouette: :e-mail: :calling:
+After generating Gmail API, the CLIENT CONFIGURATION will be saved as ***```credentials.json```***. It should be kept in the working directory. otherwise contacts details can't be fetched from Google Contacts.
+# :small_orange_diamond: token.pickle :memo:
+All the details will be saved in ***```token.pickle```*** file and no further retrieving will be occurred if there is no change.
+# :small_orange_diamond: era_ai.py :bust_in_silhouette:
+##### All the tasks (possible by Era) are following -
+1. Tells ***```'I'm fine'```*** when asked ***```how are you```***.<br><br>
+2. Tells ***```It's good to know that you are fine```*** if the word ***```fine```*** is in the query.<br><br>
+3. ***```Finds and tells related data about query```***(first two sentences) from wikipedia if ***```wikipedia```*** is in the query.<br><br>
+4. It'll say ***```I'm Era - a personal desktop assistant```*** when asked ***```who are you```***.<br><br>
+5. ***```Opens Spartan14```***(a chemistry software) if ***```spartan```*** is in the query.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The target path should be specified ***(software -> file location -> properties -> target path).***<br><br>
+6. ***```Opens YouTube```*** if ***```youtube```*** is in the query.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The target URL should be specified ***(https://www.youtube.com/)*** :link:<br><br>
+7. ***```Opens Google```*** if ***```google```*** is in the query.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: the target URL should be specified ***(https://www.google.co.in/)*** :link:<br><br>
+8. ***```Opens Stack Overflow```*** if ***```stackoverflow```*** is in the query.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The target URL should be specified ***(https://stackoverflow.com/)*** :link:<br><br>
+9. ***```Plays a random somg or changes the song```*** if ***```play song```*** or ***```change song```*** is in the query.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The local folder should be specified where the songs are kept.<br><br>
+10. Tells us the current local ***```time```*** if ***```time```*** is in the query.<br><br>
+11. ***```Opens Visual Studio Code```*** if ***```code```*** is in the query.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The target path should be specified ***(software -> file location -> properties -> target path)***.<br><br>
+12. ***```'Quits the application'```*** when ***```quit```*** or ***```close```*** or ***```exit```*** in the query.<br><br>
+13. Tells ***```'Thank you sir, I'm always here for you'```*** when ***```awesome```*** or ***```amazing```*** in the query.<br><br>
+14. ***```Searches in Google```*** about the query if ***```what```*** or ***```who```*** or ***```where```*** or ***```can you```*** in the query.<br><br>
+15. ***```Sends a mail to the contact```*** if the query matches with one of the ***```Google Contacts```***.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The ***contacts*** and associated ***email ids*** can be fetched using ***Gmail API***.
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :white_small_square: For more details about **unlimited retrieving of contact details using Gmail API** please read my article published in ***```Medium```*** - <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;https://medium.com/@aritraroycoc/retrieving-email-and-phone-no-7c60ad3a9b69 :link:
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: To send mail inbuilt module **smtplib**  is used. But one has to ***less secure apps and google account using G Suite Admin Account***.
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :white_small_square: Please note, **it is not normal Google Account and read all the terms and conditions for further approaching**.
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :white_small_square: For futher information please visit - <br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;https://support.google.com/a/answer/6260879?hl=en :link: <br><br>
+16. ***```Makes a voice call to the contact```*** if the query matches with one of the ***```Google Contacts```***.
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: The ***contacts*** and associated ***phone numbers*** can be fetched using ***Gmail API***.
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :white_small_square: For more details about **unlimited retrieving of contact details using Gmail API** please read my article published in ***```Medium```*** - <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;https://medium.com/@aritraroycoc/retrieving-email-and-phone-no-7c60ad3a9b69 :link:
+<br>&nbsp;&nbsp;&nbsp;&nbsp; :black_small_square: To make voice calls I've taken the help of ***Twilio account***.
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :white_small_square: One'll need to get ***account_sid*** and ***auth_token***. Please check all the details at - <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;https://www.twilio.com/docs/voice/tutorials/how-to-make-outbound-phone-calls-python :link:
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :white_small_square: Please note, ***One can only make call to the registered number for a limited trial period***. For full access **one'll need to pay**.
+# How to Use :question::question::question:
+### Follow These Simple Steps:
+* **Star** and **Fork** this repo to your account
+* Go to your project folder using your shell and run the following code to install all the required modules - <br> **```pip install -r requirements.txt```**
+* Now run the python code to enjoy the assistance of **Era**.
+
+## :blush::blush::blush:Contributors Are Welcome :blush::blush::blush:
+#### Step for Contribution:
+* **Star** and **Fork** this repo to your account
+* Create a **New Branch** and do **Necessary Modification**
+* Send a **PR**
+
+## Found a bug :x::x::x:
+Don' t worry I am always here to help you. Create a issue [HERE](https://github.com/aritraroy24/Voice_Assistant_Era/issues)
+
+## Loved My Work :heart_eyes::heart_eyes::heart_eyes:
+You can  help me by contributing here :point_right: <a href="https://www.buymeacoffee.com/aritraroy24" ><img align="center" src="https://www.linkpicture.com/q/buycoffee.png" width="150" /></a>
+</p>
---
+++ era_ai.py
@@ -0,0 +1,338 @@
+from __future__ import print_function
+import pyttsx3
+import datetime
+import smtplib
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import random
+from twilio.rest import Client
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import requests
+import json
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
+
+
+def speak(audio):
+    # It speaks a given string
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    # Wish according to the time.
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning!")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
+    elif hour >= 18 and hour < 23:
+        speak("Good Evening!")
+    else:
+        speak("Good Night, sir...It's good for health to have dinner and go to bed now...as you know Early to bed and early to rise, makes a man healthy, wealthy and wise.")
+        speak("Thanks for using Era")
+        exit()
+        
+    speak("I'm Era, your personal voice assistant. Please tell how may I help you?")
+
+def sendEmail(to, content):
+    # It sends an email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('your_email@gmail.com',
+                 'your_password')    # Enter your password
+    server.sendmail('your_email@gmail.com', to, content)
+    server.close()
+
+
+def fetchNameEmail():
+    """Fetches name and their email ids from google contacts"""
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('people', 'v1', credentials=creds)
+
+    # Call the People API
+    results = service.people().connections().list(
+        resourceName='people/me',
+        pageSize=1500,
+        personFields='names,emailAddresses').execute()
+    connections = results.get('connections', [])
+    name1List = []
+    emailList = []
+    for person in connections:
+        names = person.get('names', [])
+        emails = person.get('emailAddresses', [])
+
+        if names and emails:
+            name = names[0].get('displayName')
+            name1List.append(name)
+            email = emails[0]['value']
+            emailList.append(email)
+    nameEmailList = zip(name1List, emailList)
+    sortedName1List = sorted(nameEmailList, key = lambda x: x[0]) 
+    return sortedName1List
+    
+
+def name1Lower(name1List):
+    """Makes all the names lowercase for name-email id list"""
+    name1ListLowerSplit = []
+    name1ListLower = list(map(lambda x:x.lower(), name1List))
+    name1ListLowerSplit = list(map(lambda x: x.split(), name1ListLower))
+    return name1ListLowerSplit
+
+def fetchNamePhoneNo():
+    """Fetches name and their phone numbers from google contacts"""
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('people', 'v1', credentials=creds)
+
+    # Call the People API
+    results = service.people().connections().list(
+        resourceName='people/me',
+        pageSize=1500,
+        personFields='names,emailAddresses,phoneNumbers').execute()
+    connections = results.get('connections', [])
+
+    name2List = []
+    phoneNoList = []
+    for person in connections:
+        names = person.get('names', [])
+        phones = person.get('phoneNumbers', [])
+
+        if phones:
+            name = names[0].get('displayName')
+            name2List.append(name)
+            phone = phones[0]['value']
+            phoneNoList.append(phone)
+    namePhoneNoList = zip(name2List, phoneNoList)
+    sortedName2List = sorted(namePhoneNoList, key = lambda x: x[0])
+    return sortedName2List
+
+def name2Lower(name2List):
+    """Makes all the names lowercase for name-phone number list"""
+    name2ListLowerSplit = []
+    name2ListLower = list(map(lambda x:x.lower(), name2List))
+    name2ListLowerSplit = list(map(lambda x: x.split(), name2ListLower))
+    return name2ListLowerSplit
+
+def queryLowerSplit(query):
+    """Makes all the query elements lowercase"""
+    queryLower = query.lower()
+    lst = []
+    lst = queryLower.split()
+    return lst
+
+def takeCommand():
+    # It takes microphone input from user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source)
+        r.pause_threshold = 1
+        audio = r.listen(source, phrase_time_limit=4)  # it converts the audio input into string and gives a span of 4 sec to an user to speak
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        # query = r.recognize_sphinx(audio)     #instead of that we can use this is offline but accuray very poor
+        print(f"User said: {query}")
+    except:
+        print("Say that again please...")
+        return "None"
+    return query
+
+def splitWords(query):
+    return (lst[0].split())
+
+
+def givenews():
+    speak("News for today..Lets begin")
+    url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=cbdbe6b492824c9b837b508c4d0fdc92"
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    arts = news_dict['articles']
+    i = 1
+    for article in arts[:-1]:
+        speak(article['title'])
+        print(f"\n{i}. {article['title']}")
+        speak("Moving on to the next news....")
+        i+=1
+    for article in arts[-1:]:
+            speak(article['title'])
+            print(f"\n{i}. {article['title']}")
+            speak("Thanks for listening...")
+    speak("Stay tuned for more updated news")
+
+
+if __name__ == '__main__':
+    wishMe()
+    while True:
+        # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if "how are you" in query:
+            speak("I'm fine sir, what about you?")
+        elif "fine" in query:
+            speak("It's good to know that you are fine.")
+        elif "who are you" in query:
+            speak("My name is Era. I'm a desktop assistant made by Mr Aritra.")
+        elif 'wikipedia' in query:
+            # sentences=2 means return first two string
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia..")
+            # print("According to wikipedia")
+            # print(results)
+            speak(results)
+        elif 'open spartan' in query or 'spartan' in query:
+            spartanPath = "C:\\Program Files\\Wavefunction\\Spartan14v114\\WF14gui64.exe"
+            os.startfile(spartanPath)
+        elif 'open youtube' in query:
+            webbrowser.open('http://www.youtube.com')
+        elif 'open google' in query:
+            webbrowser.open('https://www.google.co.in/')
+        elif 'open stackoverflow' in query:
+            webbrowser.open('https://stackoverflow.com/')
+        elif 'play music' in query or 'play song' in query or 'play some music' in query or 'play another music' in query or 'change song' in query or 'next song' in query:
+            music_dir = 'G:\\RabindraSangeet'
+            songs = os.listdir(music_dir)
+            os.startfile(os.path.join(
+                music_dir, songs[random.randint(0, len(songs)-1)]))
+        elif 'the time' in query or 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query or 'open visual studio' in query:
+            codePath = "C:\\Users\\Aritra Roy\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'quit' in query or 'exit' in query or 'close' in query:
+            speak("Thanks for using Era!!!")
+            exit()
+
+        elif 'awesome' in query or 'wow' in query or 'amazing' in query or 'wonderful' in query:
+            speak("Thank you sir, I'm always here for you")
+
+        elif 'what' in query or 'who' in query or 'where' in query or 'can you' in query:
+            webbrowser.open(f"https://www.google.com/search?&q={query}")
+            speak(wikipedia.summary(query, sentences=2))
+
+        elif 'email to' in query or 'send a mail' in query or 'mail to' in query:
+            # This will send mail only if there is any matching name in last of query
+            # the last word should be in all strings contain a name which is exist as key in nameList
+            zippedNameEmailList = fetchNameEmail()
+            name1List, emailList = zip(*zippedNameEmailList)
+            name1FinalList = name1Lower(name1List)
+            queryList = queryLowerSplit(query)
+            i = 0
+            for item in name1FinalList:
+                i+=1
+                for item1 in item:
+                    for item2 in queryList:
+                        if item2 == item1:
+                            try:
+                                speak("What is your message ?")
+                                content = takeCommand()
+                                to = emailList[i-1]
+                                sendEmail(to, content)
+                                speak("Email has been sent")
+                                break
+                            except Exception as e:
+                                print(e)
+                                speak("Sorry sir, something went wrong and i am not able to send your email right now.")
+                                break
+                    else:
+                        continue
+                    break
+                else:
+                    continue
+                break
+            if i+1 > len(name1FinalList):
+                speak("Contact not found")
+
+        elif 'phone' in query or 'make call' in query or 'call' in query:
+            zippednamePhoneNoList = fetchNamePhoneNo()
+            name2List, phoneNoList = zip(*zippednamePhoneNoList)
+            name2FinalList = name2Lower(name2List)
+            queryList = queryLowerSplit(query)
+            i = 0
+            for item in name2FinalList:
+                i+=1
+                for item1 in item:
+                    for item2 in queryList:
+                        if item2 == item1:
+                            try:
+                                # Your Account Sid and Auth Token from twilio.com/console
+                                # DANGER! This is insecure. See http://twil.io/secure
+                                account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+                                auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+                                client = Client(account_sid, auth_token)
+
+                                call = client.calls.create(
+                                                        twiml='<Response><Say>Ahoy, World!</Say></Response>',
+                                                        to = phoneNoList[i-1],
+                                                        from_='+1XXXXXXXXXX'
+                                                    )
+                                speak("Calling has been initiated")
+                                break
+                            except Exception as e:
+                                print(e)
+                                speak("Sorry sir, something went wrong and i am not able to call right now.")
+                                break
+                    else:
+                        continue
+                    break
+                else:
+                    continue
+                break
+            if i+1 > len(name2FinalList):
+                speak("Contact not found")
+
+        elif 'headlines' in query or 'news' in query or 'headline' in query:
+            givenews()
